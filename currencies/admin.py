0d12da4cf3f9f1c6f0c1237024bb09d5c8e4c4d0
ogawryszewski <ogawryszewski@griddynamics.com>
2025-04-04 13:28:45 +0200

    add admin entites

--- currencies/admin.py
+++ currencies/admin.py
@@ -1,3 +1,22 @@
-# from django.contrib import admin
+from currencies.models import Currency, ExchangeRate, FavouriteCurrency
+from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('base', 'name', 'symbol')
+    search_fields = ('base', 'name')
+
+
+@admin.register(ExchangeRate)
+class ExchangeRateAdmin(admin.ModelAdmin):
+    list_display = ('currency', 'date', 'rate')
+    list_filter = ('date', 'currency')
+    search_fields = ('currency__base', 'currency__name')
+
+
+@admin.register(FavouriteCurrency)
+class FavoriteCurrencyAdmin(admin.ModelAdmin):
+    list_display = ('user', 'currency', 'date_added')
+    list_filter = ('user', 'currency')
+    search_fields = ('user__username', 'currency__base')
